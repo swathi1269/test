a2f9ab4230aec80808521965cc89d9f5fdadfb6c
swathi1269 <39548839+swathi1269@users.noreply.github.com>
2020-11-04 16:44:50 +0530

    test commit

--- src/test/java/loginTest.java
+++ src/test/java/loginTest.java
@@ -19,7 +19,7 @@ public class loginTest {
         driver.get("https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
 
         driver.findElement(By.xpath("//*[@id='identifierId']")).sendKeys("swathiacharya92@gmail.com");
-        driver.findElement(By.xpath("//*[@class='VfPpkd-RLmnJb']")).click();
+       // driver.findElement(By.xpath("//*[@class='VfPpkd-RLmnJb']")).click();
         driver.quit();
     }
 
